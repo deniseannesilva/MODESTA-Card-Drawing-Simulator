52faf69ab601b158ae91d822404bd07397c0f765
brosashazel <hazel_brosas@dlsu.edu.ph>
2017-08-06 13:16:19 +0800

    drawing simulator

--- CardDrawing/src/CardDrawingSimulator.java
+++ CardDrawing/src/CardDrawingSimulator.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import objects.Card;
 import objects.Deck;
 
 public class CardDrawingSimulator {
@@ -9,6 +10,7 @@ public class CardDrawingSimulator {
 	int numCards, numTrials;
 	Scanner sc;
 	Deck deck;
+	Boolean validDraw, validTrial;
 	
 	public CardDrawingSimulator(Logger log){
 		this.log = log;
@@ -16,6 +18,8 @@ public class CardDrawingSimulator {
 	}
 	
 	public void init(){
+		validDraw = true;
+		validTrial = true;
 		numCards = 0;
 		numTrials = 0;
 		deck = new Deck();
@@ -23,19 +27,66 @@ public class CardDrawingSimulator {
 	
 	public void start(){
 		sc = new Scanner(System.in);
+		while(validDraw) {
+			System.out.print("Please input number of cards (1 to 5): ");
+			numCards = sc.nextInt();
+			if(validateDraw(numCards))
+				validDraw = true;
+			else validDraw = false;
+		}
 		
-		System.out.println("Please input number of cards (1 to 5): ");
-		numCards = sc.nextInt();
-		System.out.println("Please enter number of trials: ");
-		numTrials = sc.nextInt();
-		
+		while(validTrial) {
+			System.out.print("Please enter number of trials (10 to 100000): ");
+			numTrials = sc.nextInt();
+			if(validateTrials(numTrials))
+				validTrial = true;
+			else validTrial = false;
+		}
+
 		deck.shuffle();
-		logFile(numCards, numTrials, deck);
+		drawWith(numCards, numTrials);
+		drawWithout(numCards, numTrials);
 	}
 	
-	public void logFile(int numCards, int numTrials, Deck deck){
-		for(int i = 0; i < numTrials; i++){
-			
+	public boolean validateDraw(int x) {
+		if(x > 5 || x < 1) {
+			System.out.println("Invalid Input. Try Again");
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean validateTrials(int x) {
+		if(x > 100000 || x < 1) {
+			System.out.println("Invalid Input. Try Again");
+			return true;
+		}
+		return false;
+	}
+	
+	public void drawWith(int numCards, int numTrials) {
+		ArrayList<Card> resultCards;
+		System.out.println("\nWith Replacement Results");
+		for(int i = 0; i < numTrials; i++) {
+			System.out.println("Trial # " + (i+1));
+			resultCards = deck.draw(numCards, true);
+			displayTrial(resultCards);
+		}
+	}
+	
+	public void drawWithout(int numCards, int numTrials) {
+		ArrayList<Card> resultCards;
+		System.out.println("\nWithout Replacement Results");
+		for(int i = 0; i < numTrials; i++) {
+			System.out.println("Trial # " + (i+1));
+			resultCards = deck.draw(numCards, false);
+			displayTrial(resultCards);
+		}
+	}
+	
+	public void displayTrial(ArrayList<Card> result) {
+		for(int i = 0; i < result.size(); i++) {
+			System.out.println("Card "+(i+1)+": " + result.get(i).getRank() + " " + result.get(i).getSuit());
 		}
 	}
 }
